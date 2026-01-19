980bfccb034124a7d318585c42aca94809865654
Supreeth-21 <supreethkr9@gmail.com>
2026-01-19 19:04:02 +0530

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import express from 'express'
 
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: "welcome to nodejs"})
+})
+app.listen(3000, ()=>{
+    console.log("server running on port 3000");
+})
